e8b41fd58123a60370e4fc5e184b1d193cf3aa3d
vattcarter7 <vattcarter@gmail.com>
2020-05-12 17:45:59 +0700

    fixed comment

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -28,7 +28,7 @@ const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
 
-// When to use annotations
+//  * When to use annotations
 // 1. Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
