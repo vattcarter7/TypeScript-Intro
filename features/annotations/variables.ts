f355ac0fdf7b48891bcbbaab79bd81ded258bda8
vattcarter7 <vattcarter@gmail.com>
2020-05-13 00:02:58 +0700

    functions in typescript

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -44,3 +44,13 @@ for (let i = 0; i < words.length; i++) {
     foundWord = true;
   }
 }
+
+// 3. Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
