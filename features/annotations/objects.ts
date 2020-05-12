26969dd6bb7ae96594fcac8ea6c2aeb695101bd9
vattcarter7 <vattcarter@gmail.com>
2020-05-13 00:17:13 +0700

    objects in typescript

---
+++ features/annotations/objects.ts
@@ -0,0 +1,17 @@
+const profile = {
+  name: 'alex',
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+};
+
+const { age } = profile;
+
+const {
+  coords: { lat, lng }
+}: { coords: { lat: number; lng: number } } = profile;
