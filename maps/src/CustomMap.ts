e33e3941923519b0ebafbe91e00f3042fbdac574
vattcarter7 <vattcarter@gmail.com>
2020-05-13 16:20:08 +0700

    map project with typescript

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,45 @@
+// ** the code below just to enable google global variable
+/// <reference types="@types/googlemaps" />
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker' method
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
