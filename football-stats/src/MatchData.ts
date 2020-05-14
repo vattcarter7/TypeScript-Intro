798d2ae93326a92ef55ababf9c23c4a8276cdd6a
vattcarter7 <vattcarter@gmail.com>
2020-05-14 15:56:48 +0700

    final on football analysis app with typescript

---
+++ football-stats/src/MatchData.ts
@@ -0,0 +1,12 @@
+import { MatchResult } from './MatchResult';
+
+// Tuple Type
+export type MatchData = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+];
--- football-stats/src/index.ts
+++ football-stats/src/index.ts
@@ -1,2 +1,8 @@
-console.log('Hi there');
-const a = 1;
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
+
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
