3f47229961fc611a648672445f608321e7c44e29
vattcarter7 <vattcarter@gmail.com>
2020-05-15 01:57:33 +0700

    meh

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hi there');
+import { User } from './models/User';
+
+const user = new User({ name: 'new record', age: 999 });
+
+user.events.on('change', () => {
+  console.log('hahaha');
+});
+
+user.events.trigger('change');
---
+++ web-framework/src/models/User.ts
@@ -0,0 +1,21 @@
+import { Eventing } from './Eventing';
+import { Sync } from './Snyc';
+import { Attributes } from './Attributes';
+
+export interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+const rootUrl = 'http://localhost:3000/users';
+
+export class User {
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+}
