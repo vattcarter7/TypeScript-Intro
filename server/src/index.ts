705205c6fa95971fcf651610e726e8aa2e7a109a
vattcarter7 <vattcarter@gmail.com>
2020-06-10 22:26:09 +0700

    added login routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['afasdflkjdfs'] }));
 app.use(router);
 
 app.listen(3000, () => {
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,9 +1,39 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+
+// how to deal with poor type def
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hi there');
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method='POST'>
+      <div>
+        <label>Email</label>
+        <input name='email' />
+      </div>
+
+      <div>
+      <label>Password</label>
+      <input name='password' type='password' />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    // mark this person as logged in
+    res.redirect('/');
+
+    //redirect them to the root route
+  } else {
+    res.send('Invalid email or password');
+  }
 });
 
 export { router };
