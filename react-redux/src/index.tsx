4478a657eac93b5b882b50b1875ccfe29eae5766
vattcarter7 <vattcarter@gmail.com>
2020-07-27 14:33:13 +0700

    setup react and redux with typescript

---
+++ react-redux/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return <div>Hi there</div>
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
