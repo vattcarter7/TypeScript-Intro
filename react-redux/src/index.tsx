4019305182113932ea6ccc07444fa0caa1822cfa
vattcarter7 <vattcarter@gmail.com>
2020-07-27 14:56:30 +0700

    implement functional component on typescript

--- react-redux/src/index.tsx
+++ react-redux/src/index.tsx
@@ -5,26 +5,30 @@ interface AppProps {
   color?: string;
 }
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 })
+//   }
+
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(<App color='red' />, document.querySelector('#root'))
