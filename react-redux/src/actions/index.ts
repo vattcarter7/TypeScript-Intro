01b95429f387a52cda3d0d7b0ab9d9d5340dbeb9
vattcarter7 <vattcarter@gmail.com>
2020-08-07 15:08:05 +0700

    implemented enum types in redux

--- react-redux/src/actions/index.ts
+++ react-redux/src/actions/index.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: false;
+}
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
     dispatch({
-      type: 'FETCH_TODOS',
+      type: ActionTypes.fetchTodos,
       payload: response.data
     });
   };
