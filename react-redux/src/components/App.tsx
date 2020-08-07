80c7be79ca66ed1ea89c40a8d097dc4dd8fd5298
vattcarter7 <vattcarter@gmail.com>
2020-08-07 15:33:11 +0700

    added connect func to redux

--- react-redux/src/components/App.tsx
+++ react-redux/src/components/App.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Todo, fetchTodos } from '../actions'
+import { StoreState } from '../reducers'
 
-export class App extends Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
 
+class _App extends Component<AppProps> {
   render() {
     return (
       <div>
@@ -10,3 +17,12 @@ export class App extends Component {
     )
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos }
+}
+
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos }
+)(_App);
